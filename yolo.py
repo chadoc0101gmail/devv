90d3659e72b438aff29c0c28b4bad7010a575396
chadoc0101 <101116745+chadoc0101@users.noreply.github.com>
2022-05-24 12:25:39 +0000

    Update yolo.py

--- yolo.py
+++ yolo.py
@@ -236,7 +236,8 @@ class YOLO(object):
             top_conf    = results[0][:, 4] * results[0][:, 5]
             top_boxes   = results[0][:, :4]
             #此处存在循环
-            st.subheader(':balloon:预测结果：{}     '.format(results[0][:, 6]))
+            top_label_Number = sizeof(top_label) / sizeof(top_label[0])
+            st.subheader(':balloon:预测结果：{}     '.format(top_label))
         #---------------------------------------------------------#
         #   设置字体与边框厚度
         #---------------------------------------------------------#
