e66d04f199dd1bae986397c89e22d5cea6891365
chadoc0101 <101116745+chadoc0101@users.noreply.github.com>
2022-05-24 13:23:25 +0000

    Update yolo.py

--- yolo.py
+++ yolo.py
@@ -297,7 +297,7 @@ class YOLO(object):
     ## 表格 ##len(np.unique(top_label))
         nnnnn = len(np.unique(top_label))
         print(len(np.unique(top_label)))
-        df = pd.DataFrame(data=np.zeros((nnnnn, 6)),
+        df = pd.DataFrame(data=np.zeros((3, 6)),
             columns=['危害鸟种', '置信度','先验框个数','涉鸟故障类型','风险等级','防治措施'],
             index=np.linspace(1, 3, 3, dtype=int))
         #for i, c in list(enumerate(np.unique(top_label))):    #将矩阵添加索引（键值对）
