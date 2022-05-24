55e8464daef057da16cf90caf52242199a7b2628
chadoc0101 <101116745+chadoc0101@users.noreply.github.com>
2022-05-24 13:10:51 +0000

    Update yolo.py

--- yolo.py
+++ yolo.py
@@ -296,7 +296,7 @@ class YOLO(object):
         st.subheader(':balloon:预测结果：{}     :balloon:置信度：{:.2f}'.format(ChineseName[predicted_class],score))
     ## 表格 ##len(np.unique(top_label))
         
-        df = pd.DataFrame(data=np.zeros((3, 6)),
+        df = pd.DataFrame(data=np.zeros((len(np.unique(top_label)), 6)),
             columns=['危害鸟种', '置信度','先验框个数','涉鸟故障类型','风险等级','防治措施'],
             index=np.linspace(1, 3, 3, dtype=int))
         #for i, c in list(enumerate(np.unique(top_label))):    #将矩阵添加索引（键值对）
