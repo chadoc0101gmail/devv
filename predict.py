6b84c01ff52ac630aa73e00bb55dab8af5b1f0bf
chadoc0101 <101116745+chadoc0101@users.noreply.github.com>
2022-07-09 04:43:38 +0000

    Update predict.py

--- predict.py
+++ predict.py
@@ -110,9 +110,9 @@ if __name__ == "__main__":
        
 
         elif choose == "视频识别":  
-            video_path = st.file_uploader('视频加载处')  # 上传本地视频
+            video_path = st.file_uploader('视频加载处', type=['mp4'])  # 上传本地视频
             if video_path:
-                capture = cv2.VideoCapture(video_path.getbuffer())
+                capture = cv2.VideoCapture(video_path)
                 st.title("涉鸟隐患")
                 fps = 0.0
                 while(True):
