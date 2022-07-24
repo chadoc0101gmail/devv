2ffcc0a070988048330da242bbe2abdb4dab4850
chadoc0101 <101116745+chadoc0101@users.noreply.github.com>
2022-07-24 06:36:52 +0000

    Update predict.py

--- predict.py
+++ predict.py
@@ -143,7 +143,7 @@ if __name__ == "__main__":
                     if not ref:
                         break
                     frame_counter += 1
-                    if frame_counter == 1:
+                    if frame_counter == 24:
                         frame_counter = 0
                         # 格式转变，BGRtoRGB
                         frame = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
