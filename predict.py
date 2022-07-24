1b5a4030c7526997d777047a55fed0f9fd4249cc
chadoc0101 <101116745+chadoc0101@users.noreply.github.com>
2022-07-24 07:18:40 +0000

    Update predict.py

--- predict.py
+++ predict.py
@@ -53,7 +53,7 @@ if __name__ == "__main__":
     #   保存视频时需要ctrl+c退出或者运行到最后一帧才会完成完整的保存步骤。
     #----------------------------------------------------------------------------------------------------------#
     video_path      = 0
-    video_save_path = 'rt_out.avi'
+    video_save_path = 'rt_out.mp4'
     video_fps       = 25.0
     #-------------------------------------------------------------------------#
     #   test_interval用于指定测量fps的时候，图片检测的次数
@@ -124,7 +124,7 @@ if __name__ == "__main__":
                 capture = cv2.VideoCapture(tfile.name)
                 #保存视频
                 
-                fourcc = int(cv2.VideoWriter_fourcc(*'xvid'))  # 选择编码方式 streamlit不支持显示MPV4编码方式
+                fourcc = int(cv2.VideoWriter_fourcc(*'x264'))  # 选择编码方式 streamlit不支持显示MPV4编码方式
                 width = int(capture.get(cv2.CAP_PROP_FRAME_WIDTH))  # 获取视频图像宽
                 height = int(capture.get(cv2.CAP_PROP_FRAME_HEIGHT))  # 获取视频图像高
                 fps = capture.get(cv2.CAP_PROP_FPS)  # 读取图像显示帧率
