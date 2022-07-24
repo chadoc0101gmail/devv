0b7b24cd055817506b6fdc88f1ff76ccda895b89
chadoc0101 <101116745+chadoc0101@users.noreply.github.com>
2022-07-24 06:06:48 +0000

    Update predict.py

--- predict.py
+++ predict.py
@@ -157,11 +157,10 @@ if __name__ == "__main__":
                     
                 yolo.show_df()
                 capture.release()
-                if video_save_path!="":
-                    #播放视频
-                    st.title("以下是识别后视频")
-                    st.video(video_save_path)
-#                     out.release()
+                #播放视频
+                st.title("以下是识别后的视频")
+                st.video(outfile_name)
+                out.release()
                     #图片转化为视频
 #                         if video_save_path!="":
 #                             print("Save processed video to the path :" + video_save_path)
